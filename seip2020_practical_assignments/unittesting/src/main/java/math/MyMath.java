7481ac6c4edad8846aa7354784c42e29d3980718
yriasykioti <44656185+yriasykioti@users.noreply.github.com>
2020-04-23 01:00:18 +0300

    Add a new method isPrime in the MyMath class

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -13,10 +13,10 @@ public class MyMath {
 
 	/**
 	 * Performs the basic arithmetic operation of factorial
+	 * 
 	 * @param n the number of the operation
 	 * @return the result of the factorial of the number n
-	 * @exception IllegalArgumentException
-	 *                when parameter n is a negative number or when n>12
+	 * @exception IllegalArgumentException when parameter n is a negative number or when n>12
 	 */
 	public int factorial(int n) {
 		int factorial = 1;
@@ -27,6 +27,29 @@ public class MyMath {
 				factorial = factorial * i;
 			}
 			return factorial;
+		}
+	}
+
+	/**
+	 * Checks if a number is prime or not.
+	 * @param n the number to be tested if it is prime or not
+	 * @return true if the number n is prime and false if the number n is not prime
+	 * @exception IllegalArgumentException when parameter n is below 2
+	 */
+
+	public boolean isPrime(int n) {
+		if (n < 2) {
+			throw new IllegalArgumentException("The input number should be >=2");
+		} else {
+			boolean flag = true;
+			for (int i = 2; i <= n / 2; ++i) {
+				// condition for nonprime number
+				if (n % i == 0) {
+					flag = false;
+					break;
+				}
+			}
+			return flag;
 		}
 	}
 
