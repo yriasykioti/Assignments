24c4beac21a0a4405103c01fd8565f58e28d1811
yriasykioti <44656185+yriasykioti@users.noreply.github.com>
2020-04-30 23:27:32 +0300

    Add an else if statement in order to throw an exception if y=0

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArithmeticOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArithmeticOperations.java
@@ -1,5 +1,7 @@
 package math;
 
+
+
 /**
 * The MathOperations provides simple arithmetic operations
 * that serve as hands-on practice on Unit Testing.
@@ -36,12 +38,16 @@ public class ArithmeticOperations {
 	public int multiply(int x, int y) {
 		if (x < 0 || y < 0) {
 			throw new IllegalArgumentException("x & y should be >= 0");
+		}else if(y==0) {
+			throw new IllegalArgumentException("y shouldn't be zero");
 		} else if (x <= Integer.MAX_VALUE/y) {
 			return x*y;			
 		} else {
 			throw new IllegalArgumentException("The product does not fit in an Integer variable");		
 		}
 	}
+	
+	
 	
 	
 }
